1b2ad985a2dcd7cb5e91f1c058931ed15f9cdba1
taey1344 <xodus1344@gmail.com>
2020-10-12 23:09:51 +0900

    Video Form 완성!

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -1,12 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Typography, Button, Form, message, Input, Icon } from 'antd';
 import DropZone from 'react-dropzone';
 
 const { TextArea } = Input;
 const { Title } = Typography;
 
+
+const PrivateOptions = [
+  { value: 0, label: "Private" },
+  { value: 1, label: "Public" }
+]
+
+const CategoryOptions = [
+  { value: 0, label: "Film & Animation" },
+  { value: 1, label: "Autos & Vehicles" },
+  { value: 2, label: "Music" },
+  { value: 3, label: "Pets & Animals" },
+]
+
 function VideoUploadPage() {
 
+  // state 설정
+  const [VideoTitle, setVideoTitle] = useState("")
+  const [Description, setDescription] = useState("")
+  const [Private, setPrivate] = useState(0)    // Private -> 0, Public -> 1
+  const [Category, setCategory] = useState("Film & Animation") // 초기값
+
+  // 이벤트 설정
+  const onTitleChange = (e) => {
+    //console.log(e.currentTarget)
+    // state를 바꾸려면 set~~된것을 바꾸면 된다.
+    setVideoTitle(e.currentTarget.value)
+  }
+
+  const onDescriptionChange = (e) => {
+    setDescription(e.currentTarget.value)
+  }
+
+  const onPrivateChange = (e) => {
+    setPrivate(e.currentTarget.value)
+  }
+
+  const onCategoryChange = (e) => {
+    setCategory(e.currentTarget.value)
+  }
 
 
   return (
@@ -45,25 +82,36 @@ function VideoUploadPage() {
         <br />
 
         <label>Title</label>
-        <Input onChange value />
+        <Input
+          onChange={onTitleChange}
+          value={VideoTitle}
+        />
 
         <br />
         <br />
         <label>Description</label>
-        <TextArea onChange value />
+        <TextArea
+          onChange={onDescriptionChange}
+          value={Description}
+        />
 
         <br />
         <br />
 
-        <select onChange>
-          <option key value></option>
+        <select onChange={onPrivateChange}>
+          {PrivateOptions.map((item, index) => (
+            <option key={index} value={item.value}>{item.label}</option> // map을 쓰면 항상 key가 있어야 에러가 안뜸
+          ))}
         </select>
 
         <br />
         <br />
 
-        <select onChange>
-          <option key value></option>
+        <select onChange={onCategoryChange}>
+          {CategoryOptions.map((item, index) => (
+            <option key={index} value={item.value}>{item.label}</option>
+          ))}
+
         </select>
 
         <br />
