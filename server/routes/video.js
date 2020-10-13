2a98c83fd31239567b5a26b07856990b3a7ff1a5
taey1344 <xodus1344@gmail.com>
2020-10-13 22:03:11 +0900

    랜딩 페이지에 DB에서 가져온 비디오 출력하기

--- server/routes/video.js
+++ server/routes/video.js
@@ -6,7 +6,7 @@ const { auth } = require("../middleware/auth");
 const multer = require('multer');
 var ffmpeg = require('fluent-ffmpeg');
 
-// config 옵션
+// STORAGE MULTER config 옵션
 let storage = multer.diskStorage({
   // 파일을 어디에 저장할지 설명해주는 것 -> uplodas라는 폴더에 저장
   destination: (req, file, cb) => {
@@ -18,11 +18,11 @@ let storage = multer.diskStorage({
   },
   // 어떤 형식만 가능하다고 설정 (ext !== '.mp4' || ext !== '.png') 식으로 설정가능
   fileFilter: (req, file, cb) => {
-    const ext = path.extname(file.originalname)
+    const ext = path.extname(file.originalname);
     if (ext !== '.mp4') {
       return cb(res.status(400).end('only jpg, png, mp4 is allowed'), false);
     }
-    cb(null, true)
+    cb(null, true);
   }
 });
 
@@ -44,7 +44,7 @@ router.post('/uploadfiles', (req, res) => {
 
   })
 
-})
+});
 
 router.post('/uploadVideo', (req, res) => {
 
@@ -57,7 +57,21 @@ router.post('/uploadVideo', (req, res) => {
     res.status(200).json({ success: true })
   })
 
-})
+});
+
+router.get('/getVideos', (req, res) => {
+
+  // 비디오를 DB에서 가져와 클라이언트에 보낸다.
+  // find -> 모든 데이터 가져온다.
+  Video.find()
+    .populate('writer')
+    .exec((err, videos) => {
+      if (err) return res.status(400).send(err);
+      return res.status(200).json({ success: true, videos });
+    });
+
+
+});
 
 
 router.post('/thumbnail', (req, res) => {
